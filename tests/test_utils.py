8ed752c24291c919a4b0142065c8ebe87cab3dad
jussi <jussi.h.a.martin@gmail.com>
2021-04-13 02:25:56 +0300

    Fix indents & blank lines to comply with PEP8.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,18 +6,19 @@ from utils import rotate_all
 sq = 1/np.sqrt(2)
 testdata0 = [
     (np.array([[0., 1.], [1., 0.], [0., -1.]], dtype=float), np.pi,
-    np.array([[0., -1.], [-1., 0.], [0., 1.]], dtype=float)),
+     np.array([[0., -1.], [-1., 0.], [0., 1.]], dtype=float)),
     (np.array([[0., 1.], [1., 0.], [0., -1.]], dtype=float), -np.pi,
-    np.array([[0., -1.], [-1., 0.], [0., 1.]], dtype=float)),
+     np.array([[0., -1.], [-1., 0.], [0., 1.]], dtype=float)),
     (np.array([[0., 1.], [1., 0.], [0., -1.]], dtype=float), np.pi/2,
-    np.array([[-1., 0.], [0., 1.], [1., 0.]], dtype=float)),
+     np.array([[-1., 0.], [0., 1.], [1., 0.]], dtype=float)),
     (np.array([[0., 1.], [1., 0.], [0., -1.]], dtype=float), -np.pi/2,
-    np.array([[1., 0.], [0., -1.], [-1., 0.]], dtype=float)),
+     np.array([[1., 0.], [0., -1.], [-1., 0.]], dtype=float)),
     (np.array([[0., 1.], [1., 0.], [0., -1.]], dtype=float), np.pi/4,
-    np.array([[-sq, sq], [sq, sq], [sq, -sq]], dtype=float)),
+     np.array([[-sq, sq], [sq, sq], [sq, -sq]], dtype=float)),
     (np.array([[0., 1.], [1., 0.], [0., -1.]], dtype=float), -np.pi/4,
-    np.array([[sq, sq], [sq, -sq], [-sq, -sq]], dtype=float))
+     np.array([[sq, sq], [sq, -sq], [-sq, -sq]], dtype=float))
     ]
+
 
 @pytest.mark.parametrize("vectors,angle,expected", testdata0)
 def test_rotate_all(vectors, angle, expected):
