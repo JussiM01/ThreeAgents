e7bebd3a087cfb0960932b4f9fc5ca29fdf970ea
jussi <jussi.h.a.martin@gmail.com>
2021-06-07 23:59:15 +0300

    Change default config-file name to the new format.

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-f', '--conf_file', type=str, default='visuals.json')
+    parser.add_argument(
+        '-f', '--conf_file', type=str, default='visuals_cen.json')
     parser.add_argument('-r', '--random_seed', type=int, default=0)
     parser.add_argument('-x0', '--x_min', type=float, default=0.1)
     parser.add_argument('-x1', '--x_max', type=float, default=0.2)
